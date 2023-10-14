a1294067051b41ab87024e478a9bd0c341900c72
IOANNVOLZHSKIY <ivantokarevd@gmail.com>
2023-10-15 02:33:52 +0300

    Added first elements parser for Glushkov's construction algorithm

--- 2_Lab/lab2/src/main/scala/glushkov.scala
+++ 2_Lab/lab2/src/main/scala/glushkov.scala
@@ -1,16 +1,39 @@
-type State = Int
-type StateTransitions = Map[Char, List[State]]
-
-class Machine {
-  var StartState = Int
-  var FinalStates = List[State]
-  var Transitions = Map[State, StateTransitions]
-  var StateCounter = Int
-}
+import jdk.dynalink.linker.support.Guards.isInstance
+import linearParser._
 
 object glushkov {
+  def first(regex: String): (Vector[Any], Boolean) = { // предусмотреть второй случай, где нужно вывести без булеан
+    if (regex.isInstanceOf[String]) {
+      return (Vector(regex), false)
+    } else if (regex.length == 1) {
+      return first(regex(0).toString)
+    } else if ((regex.length == 2) && (regex(1) == '*')) {
+      return (first(regex(0).toString)._1, true)
+    } else if ((regex.length >= 3) && (regex(1) == '&')) {
+      val res = first(regex(0).toString)
+      val start = res._1
+      val isKellie = res._2
 
-  def BuildMachine(): Unit ={
+      if (isKellie) {
+        val res1 = first(regex.slice(2, regex.length))
+
+        return (res1._1, res1._2)
+      } else {
+        return (start, false)
+      }
+    } else {
+      var res = Vector[Any]()
+      var isKellie = false
+
+      for (i <- 0 until regex.length by 2) {
+        val temp = first(regex(i).toString)
+
+        res = res :+ temp._1
+        val isKellie = temp._2
+      }
+
+      return (res, isKellie)
+    }
   }
 
 }
