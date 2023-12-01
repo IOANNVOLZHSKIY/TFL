3487882afd7372bbce77178a872956d3fee043bf
IOANNVOLZHSKIY <ivantokarevd@gmail.com>
2023-12-01 14:59:47 +0300

    Fix of Regex Generator

--- 2_Lab/lab2/src/main/scala/regexGenerator.scala
+++ 2_Lab/lab2/src/main/scala/regexGenerator.scala
@@ -3,9 +3,9 @@ import scala.util.Random
 object regexGenerator {
 
   def starKellie(expr: String): String = {
-    if (expr.size > 1) {
+    if (expr.length > 1) {
       return "(" + expr + ")" + "*"
-    } else if (expr.size == 1) {
+    } else if (expr.length == 1) {
       return expr + "*"
     } else {
       return ""
@@ -24,26 +24,31 @@ object regexGenerator {
     }
   }
 
-  def recursionComp(letterNumber: Int, alphabetSize: Int): String = {
-    if ((letterNumber <= 0) || (alphabetSize <= 0)) {
+  def recursionComp(maxLength: Int, maxStarHeight: Int, alphabetSize: Int): String = {
+    if ((maxLength <= 0) || (maxStarHeight <= 0)) {
       return ""
+    } else if (maxLength == 1) {
+      val letter = getLetter(Random.nextInt(alphabetSize))
+      return letter
     } else {
       val op = Random.nextInt(4)
+      val lenL = Random.nextInt(maxLength)
+      val lenR = maxLength - lenL
 
       if (op == 0) {
-        val l = recursionComp(letterNumber / 2, alphabetSize)
-        val r = recursionComp(letterNumber / 2, alphabetSize)
+        val l = recursionComp(lenL, maxStarHeight, alphabetSize)
+        val r = recursionComp(lenR, maxStarHeight, alphabetSize)
         return l + r
       } else if (op == 1) {
-        val l = recursionComp(letterNumber - 1, alphabetSize)
-        val r = recursionComp(letterNumber - 1, alphabetSize)
+        val l = recursionComp(lenL, maxStarHeight, alphabetSize)
+        val r = recursionComp(lenR, maxStarHeight, alphabetSize)
         return Alt(l, r)
       } else if (op == 2) {
-        val subExpr = recursionComp(letterNumber - 1, alphabetSize - 1)
-        return starKellie(subExpr)
-      } else {
         val letter = getLetter(Random.nextInt(alphabetSize))
         return letter
+      } else {
+        val subExpr = recursionComp(maxLength - 1, maxStarHeight - 1, alphabetSize)
+        return starKellie(subExpr)
       }
     }
   }
@@ -52,7 +57,7 @@ object regexGenerator {
     var regexes = Array.ofDim[String](regexNumber)
 
     for (i <- 0 until regexNumber) {
-      regexes(i) = recursionComp(letterNumber, alphSize)
+      regexes(i) = recursionComp(letterNumber, starHeight, alphSize)
     }
 
     return regexes
